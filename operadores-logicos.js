2033dfcf5ddaea310b08c610e7735160d8afa172
Cauê Netto <caue.netto234@gmail.com>
2024-03-20 11:30:24 -0300

    operadores lógicos

---
+++ operadores-logicos.js
@@ -0,0 +1,16 @@
+const notaFinal = 6;
+const faltas = 2;
+const advertencias = 0;
+
+if (notaFinal < 7 && faltas > 4) {
+    console.log('reprovado, boas festas');
+} else {
+    console.log('não foi reprovado por falta');
+}
+
+if(faltas >= 2 && !advertencias) {
+    console.log('Recebeu bônus');
+} else {
+    console.log('Não recebeu bônus');
+}
+
