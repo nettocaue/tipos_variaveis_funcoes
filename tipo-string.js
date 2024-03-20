3df901ac76bbc8155c15ebcba76a9706bd00635a
Cauê Netto <caue.netto234@gmail.com>
2024-03-20 11:34:16 -0300

    tipo string

---
+++ tipo-string.js
@@ -0,0 +1,14 @@
+const estudante = "Caroline";
+const docente = 'Ana';
+const cumprimento = "nosso lema é 'estudar bastante!'";
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log('a estudante chama ${estudante}');
+
+// template string
+
+
+const senha = 'SenhaSegura123' + estudante.toUpperCase();
+console.log(senha);
