8b8b2dd900068d30ad912ba00d14bed2780341bc
Cauê Netto <caue.netto234@gmail.com>
2024-03-20 11:28:21 -0300

    funções

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+// parâmetros/argumentos
+// retorno
+
+console.log(exibeInfosEstudante('Caroline', 10));
+console.log(exibeInfosEstudante('Ana', 7));
+
+function exibeInfosEstudante(nome, nota) {
+   return(`o nome é ${nome} e a nota é ${nota}`); 
+}
+
+//hoisting
